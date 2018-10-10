903b0e860750d3a13d7124e1785c9e9ec41102a8
280520@o365.stud.umk.pl <280520@o365.stud.umk.pl>
2018-10-10 21:35:32 +0200

    Iloczyn Hadamarda dwóch macierzy

--- src/hadamard.cpp
+++ src/hadamard.cpp
@@ -2,10 +2,7 @@
 
 
 double** hadamard(double **A, double **B,double **AB,int m,int n)
-{
-			
-			//double c=0;
-			
+{			
 			for(int i=0;i<m;i++)
 			{
 				for(int j=0;j<n;j++)
@@ -15,10 +12,6 @@ double** hadamard(double **A, double **B,double **AB,int m,int n)
 						AB[i][j]=A[i][j]*A[i][j];
 				}
 			}
-			
-			//A[m][n]=rand()%20;
-			//B[m][n]=rand()%20;
-			//c=A[m][n]*A[m][n];
 			
 			return AB;
 }
